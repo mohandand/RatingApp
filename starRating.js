1651a4fc86063ff5be0e64c0146c839fb655e1f3
mohandand <mohandand@gmail.com>
2023-01-26 11:13:27 -0600

    2 files modified

--- starRating.js
+++ starRating.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const RatingApp = () => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
   return (
     <div className="star-rating">
-      {[...Array(5)].map((star) => {
-        return <span className="star">&#9733;</span>;
+      {[...Array(5)].map((star, index) => {
+        return (
+          <button
+            type="text"
+            key={index}
+            className={index <= rating ? 'on' : 'off'}
+          >
+            {' '}
+            <span className="star">&#9733;</span>
+          </button>
+        );
       })}
     </div>
   );
